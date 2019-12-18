9c852bdce3016b6b73e4b21f2c26ca06e372223b
min9nim <min9nim@gmail.com>
2019-12-18 10:23:03 +0900

    update

--- test/get-new-name.spec.js
+++ test/get-new-name.spec.js
@@ -7,6 +7,6 @@ describe('getNewName', () => {
     expect(getNewName('abc', ['abc(1)', 'abc(2)', 'ddw'])).to.be.equal('abc')
     expect(getNewName('abc', ['abc(1)', 'abc(2)', 'abc'])).to.be.equal('abc(3)')
     expect(getNewName('abc', ['abc', 'abc(2)', 'abc(3)', 'abc(5)'])).to.be.equal('abc(4)')
-    expect(getNewName('abc', ['abc', 'abc(2)', 'abc(3)', , 'abc(4)', 'abc(5)', 'abc(6)', 'abc(7)', 'abc(8)', 'abc(9)', 'abc(10)'])).to.be.equal('abc(11)')
+    expect(getNewName('abc', ['abc', 'abc(2)', 'abc(3)', 'abc(4)', 'abc(5)', 'abc(6)', 'abc(7)', 'abc(8)', 'abc(9)', 'abc(10)'])).to.be.equal('abc(11)')
   })
 })
